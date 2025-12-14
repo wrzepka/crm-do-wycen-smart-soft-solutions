ef9d880481b509ab9a6df33cc78f11cf0fa9e2c2
wrzepka <90323901+wrzepka@users.noreply.github.com>
2025-12-14 02:30:57 +0100

    develop employee fetch data function

---
+++ src/lib/data/employee.ts
@@ -0,0 +1,29 @@
+import { prisma } from '@/lib/prisma-client';
+// import { getServerSession } from 'next-auth';
+// import { authOptions } from '@/lib/auth';
+
+export async function getEmployees() {
+  // const session = getServerSession();
+
+  /*  if (!session) { // in future check also role!
+      return [];
+  }*/
+
+  try {
+    const employees = await prisma.employees.findMany({
+      orderBy: { last_name: 'asc' },
+      include: {
+        employee_technology: {
+          include: {
+            technologies: true,
+          },
+        },
+      },
+    });
+
+    return employees;
+  } catch (error) {
+    console.error('Błąd pobierania pracowników:', error); // debug log
+    return [];
+  }
+}
