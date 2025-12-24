db9968d471b498055b347ca1895018fce0118c2e
wrzepka <90323901+wrzepka@users.noreply.github.com>
2025-12-24 17:30:31 +0100

    Implement pagination into clients page. translate 'next', 'previous' buttons

--- src/app/(dashboard)/dashboard/clients/page.tsx
+++ src/app/(dashboard)/dashboard/clients/page.tsx
@@ -1,5 +1,6 @@
 import { ClientListTable } from '@/components/shared/ClientListTable';
 import { clients } from '@/generated/prisma/client';
+import { DataTablePagination } from '@/components/shared/DataPagination';
 
 // Mock Data for testing only
 const mockClients: clients[] = [
@@ -29,9 +30,25 @@ const mockClients: clients[] = [
   },
 ];
 
-export default async function ClientsPage() {
+export default async function ClientsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string }>;
+}) {
+  // Get parameters
+  const sParams = await searchParams;
+  // Get current page, otherwise set it to 1
+  const currentPage = Math.max(1, Number(sParams.page) || 1);
+  // Amount of rows per page
+  const pageSize = 1;
   // after tests: const data = await getClientsList();
   const data = mockClients;
+  const totalPages = Math.ceil(data.length / pageSize);
+
+  // Calculate starting index and get part of the data to show them inside the table
+  const startIndex = (currentPage - 1) * pageSize;
+  // To change in future. To delete I think. Slicing is inside of the getClientsList()
+  const slicedData = data.slice(startIndex, startIndex + pageSize);
 
   return (
     <div className="space-y-6 p-8">
@@ -44,7 +61,12 @@ export default async function ClientsPage() {
         </div>
       </div>
 
-      <ClientListTable data={data} />
+      <ClientListTable data={slicedData} />
+
+      {/*Pagination component*/}
+      <div className="mt-4 flex justify-center">
+        <DataTablePagination currentPage={currentPage} totalPages={totalPages} />
+      </div>
     </div>
   );
 }
--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -63,7 +63,7 @@ function PaginationPrevious({ className, ...props }: React.ComponentProps<typeof
       {...props}
     >
       <ChevronLeftIcon />
-      <span className="hidden sm:block">Previous</span>
+      <span className="hidden sm:block">Poprzednia</span>
     </PaginationLink>
   );
 }
@@ -76,7 +76,7 @@ function PaginationNext({ className, ...props }: React.ComponentProps<typeof Pag
       className={cn('gap-1 px-2.5 sm:pr-2.5', className)}
       {...props}
     >
-      <span className="hidden sm:block">Next</span>
+      <span className="hidden sm:block">Następna</span>
       <ChevronRightIcon />
     </PaginationLink>
   );
@@ -91,7 +91,7 @@ function PaginationEllipsis({ className, ...props }: React.ComponentProps<'span'
       {...props}
     >
       <MoreHorizontalIcon className="size-4" />
-      <span className="sr-only">More pages</span>
+      <span className="sr-only">Więcej stron</span>
     </span>
   );
 }
