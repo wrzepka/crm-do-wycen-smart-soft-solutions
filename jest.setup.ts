a0d090f1693d6cf9163f1b59367cce5828a8d598
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-01-03 16:35:56 +0100

    jest.setup.ts config

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,28 @@
+// jest.setup.ts
+
+// Matchers extension (for shadcn/ui testing)
 import '@testing-library/jest-dom';
+
+// Polyfill for TextEncoder/TextDecoder
+// For NextAuth and Zod
+import { TextEncoder, TextDecoder } from 'util';
+
+Object.assign(global, { TextEncoder, TextDecoder });
+
+// 3. Mocking Next.js Navigation
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+      back: jest.fn(),
+    };
+  },
+  usePathname() {
+    return '';
+  },
+  useSearchParams() {
+    return new URLSearchParams();
+  },
+}));
