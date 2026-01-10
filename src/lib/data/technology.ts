17e8dd91c8f2090251e9d4ccbdf6794eefa243f6
Wiktor Rzepka <90323901+wrzepka@users.noreply.github.com>
2026-01-10 19:57:04 +0100

    Feat/filters (#43)
    * add filters checking to the lists to improve readability
    
    * Add SeachBar component for positions subview
    
    * Add SeachBar component for technologies subview
    
    * Remove invalid parameters for useIsFiltered() hook inside list tables.

--- src/lib/data/technology.ts
+++ src/lib/data/technology.ts
@@ -18,22 +18,47 @@ export async function getTechnologies(): Promise<technologies[]> {
   }
 }
 
-export async function getTechnologiesWithCount() {
+export async function getTechnologiesWithCount(
+  query: string = '',
+  page: number = 1,
+  pageSize: number = 25,
+) {
+  const skip = (page - 1) * pageSize;
+  const whereClause = {
+    AND: [
+      query
+        ? {
+            name: { contains: query, mode: 'insensitive' as const },
+          }
+        : {},
+    ],
+  };
+
   try {
-    const technologiesList = await prisma.technologies.findMany({
-      orderBy: { name: 'asc' },
-      select: {
-        id: true,
-        name: true,
-        _count: {
-          select: {
-            employee_technology: true,
+    const [technologiesList, technologiesAmount] = await Promise.all([
+      prisma.technologies.findMany({
+        where: whereClause,
+        skip: skip,
+        take: pageSize,
+
+        orderBy: { name: 'asc' },
+        select: {
+          id: true,
+          name: true,
+          _count: {
+            select: {
+              employee_technology: true,
+            },
           },
         },
-      },
-    });
+      }),
+      prisma.technologies.count({ where: whereClause }),
+    ]);
 
-    return technologiesList;
+    return {
+      technologiesList,
+      totalPages: Math.ceil(technologiesAmount / pageSize),
+    };
   } catch (err) {
     console.log('Błąd podczas pobierania technologii: ', err);
     throw new Error('Nie udało się załadować listy technologii. Spróbuj odświeżyć stronę.');
