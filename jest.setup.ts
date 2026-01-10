75090534916a29e1d7a527d383b16403c583ccd3
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-01-10 14:06:11 +0100

    Configure scripts to use test db for integration tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,3 @@
-// jest.setup.ts
-
 // Matchers extension (for shadcn/ui testing)
 import '@testing-library/jest-dom';
 
@@ -8,6 +6,19 @@ import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
 
 Object.assign(global, { TextEncoder, TextDecoder });
+
+// URL of DB for tests
+const dbUrl = process.env.DATABASE_URL;
+if (!dbUrl) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
+// If DB name does not contain 'test' and database server is not located locally, STOP THE TESTS
+if (!dbUrl.includes('test') && !dbUrl.includes('localhost')) {
+  console.error('UWAGA! Próbujesz uruchomić testy na bazie produkcyjnej/deweloperskiej!');
+  console.error(`Aktualny URL: ${dbUrl}`);
+  throw new Error('Testy muszą być uruchamiane na bazie testowej!');
+}
 
 // 3. Mocking Next.js Navigation
 jest.mock('next/navigation', () => ({
