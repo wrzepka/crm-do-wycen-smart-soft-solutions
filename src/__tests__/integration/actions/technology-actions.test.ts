a4fc8960d8c7722fd1bfb45df8fce362afc150ab
wrzepka <90323901+wrzepka@users.noreply.github.com>
2026-02-10 20:12:06 +0100

    Create integration tests for technologies

---
+++ src/__tests__/integration/actions/technology-actions.test.ts
@@ -0,0 +1,94 @@
+/**
+ * @jest-environment node
+ */
+
+import { clearDatabase, seedDatabase } from '@/__tests__/utils';
+import { prisma } from '@/lib/prisma-client';
+import {
+  createTechnology,
+  deleteTechnology,
+  setEmployeeTechnologies,
+  updateTechnology,
+} from '@/lib/actions/technology-actions';
+
+describe('Technology actions (Integration)', () => {
+  // close connection after every test
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+  // clear db after every test
+  beforeEach(async () => {
+    await clearDatabase();
+  });
+
+  describe('createTechnology', () => {
+    it('should create a new Technology', async () => {
+      const formData = new FormData();
+
+      formData.append('name', 'React');
+
+      const result = await createTechnology(formData);
+
+      expect(result.ok).toBe(true);
+    });
+  });
+
+  describe('updateTechnology', () => {
+    it('should update existing Technology', async () => {
+      const formData = new FormData();
+
+      formData.append('name', 'TS');
+
+      const seedResult = await seedDatabase();
+      const result = await updateTechnology(seedResult.reactId, formData);
+
+      expect(result.ok).toBe(true);
+
+      const findTech = await prisma.technologies.findFirst({
+        where: {
+          name: 'TS',
+        },
+      });
+
+      expect(findTech).not.toBeNull();
+      expect(findTech?.id).toEqual(seedResult.reactId);
+    });
+  });
+
+  describe('deleteTechnology', () => {
+    it('should delete existing Technology', async () => {
+      const newTech = await prisma.technologies.create({
+        data: {
+          name: 'TS',
+        },
+      });
+
+      const result = await deleteTechnology(newTech.id);
+      expect(result.ok).toBe(true);
+
+      const findTech = await prisma.technologies.findFirst({
+        where: {
+          name: 'React',
+        },
+      });
+
+      expect(findTech).toBeNull();
+    });
+  });
+
+  describe('setTechnology', () => {
+    it('should set technology to the employee', async () => {
+      const seedResult = await seedDatabase();
+      const newTech = await prisma.technologies.create({
+        data: {
+          name: 'TS',
+        },
+      });
+
+      const techArray = [newTech.id];
+
+      const result = await setEmployeeTechnologies(seedResult.seniorId, techArray);
+      expect(result.ok).toBe(true);
+    });
+  });
+});
